c0f0d25c59f519dbd98e4b04a9738f1b8f66b1d5
DeVaris Brown <devaris@devaris.com>
2012-07-11 18:45:33 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apipage::Application.config.secret_token = 'b36868f1e1887c972c36360ec46d7b37b73f21016ad5414a8a3d8767aa3814712f1a921baf58504c1ae1e57783832e412a9048211a3f5c9705944d731d2b821a'
